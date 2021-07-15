11a26b87beeb61271cea73c21672ec926ee5f6b6
Kent-Khang <vinhkhang1969@gmail.com>
2021-07-16 02:10:23 +0700

    fix some

--- js/scripts_popup.js
+++ js/scripts_popup.js
@@ -10,14 +10,3 @@ $("#txtAboutData").click(function() {
 	$("#viewImage").attr("src", "");
 	$("#viewModal").modal("toggle");
 });
-
-// Zoom Flag
-$("body").on("click", ".flagCountry", (e) => {
-	let Img = e.target.getAttribute("src"); e.preventDefault();
-	let nameCountry = e.target.parentNode.parentNode.cells[1].innerHTML;
-	document.getElementById("nameCountry").textContent = nameCountry;
-	document.getElementById("aboutTheData").textContent = "";
-	$("#viewImage").attr("src", Img);
-	$("#imgCfmCovid").attr("src", "");
-	$("#viewModal").modal("toggle");
-});
