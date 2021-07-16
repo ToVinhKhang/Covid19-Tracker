3ac42e623ebaf6d51755a6c5e39dde69d49afc9c
Kent-Khang <vinhkhang1969@gmail.com>
2021-07-16 10:52:51 +0700

    change

--- js/scripts_translation.js
+++ js/scripts_translation.js
@@ -73,8 +73,8 @@ $("#SwitchLanguages").change(function(){
 			vnName.innerHTML = dataEN.vnName;
 			nameAbData.textContent = dataEN.AboutTheData.title;
 			aboutTheData.innerHTML = dataEN.AboutTheData.content;
-			nameVaccine.textContent = dataEN.AboutTheData.title;
-			aboutTheVaccine.innerHTML = dataEN.AboutTheData.content;
+			nameVaccine.textContent = dataEN.AboutTheVaccine.title;
+			aboutTheVaccine.innerHTML = dataEN.AboutTheVaccine.content;
 		}).catch(e => console.log(e));
 	}
 	
@@ -112,8 +112,8 @@ $("#SwitchLanguages").change(function(){
 			vnName.innerHTML = dataVN.vnName;
 			nameAbData.textContent = dataVN.AboutTheData.title;
 			aboutTheData.innerHTML = dataVN.AboutTheData.content;
-			nameVaccine.textContent = dataVN.AboutTheData.title;
-			aboutTheVaccine.innerHTML = dataVN.AboutTheData.content;
+			nameVaccine.textContent = dataVN.AboutTheVaccine.title;
+			aboutTheVaccine.innerHTML = dataVN.AboutTheVaccine.content;
 		}).catch(e => console.log(e));
 	}
 });
