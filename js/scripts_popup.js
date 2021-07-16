0c8ff8d1f6c5e86d4e0fc37af054f2a66f82d487
Kent-Khang <vinhkhang1969@gmail.com>
2021-07-16 12:25:02 +0700

    Add About Vaccine Info

--- js/scripts_popup.js
+++ js/scripts_popup.js
@@ -12,11 +12,14 @@ $("#txtAboutData").change(function(){
 		$("#imgCfmCovid").attr("src", "https://img.icons8.com/bubbles/2x/question-mark.png");
 		$("#viewModalData").modal("toggle");
 		$(this).val('');
+		$(document).keydown(function(e){if(e.keyCode == 27){$('#viewModalData').hide();}});
 	}
 	// About Vaccine
 	if(target=="Vaccine"){
 		$("#imgVaccine").attr("src", "https://cms.wellcome.org/sites/default/files/styles/image_full_hi/public/2021-01/FINAL_ExplainerThumbnails_Ex9_What_Different_Types_Of_Covid_Vaccine_Are_There-1600x900.png?itok=uHtvnnMM");
 		$("#viewModalVaccine").modal("toggle");
 		$(this).val('');
+		$(document).keydown(function(e){if(e.keyCode == 27){$('#viewModalVaccine').hide();}});
 	}
 });
+
