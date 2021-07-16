561081b9cc9af0bbe1a803c6603f841431b7fb2a
Kent-Khang <vinhkhang1969@gmail.com>
2021-07-16 17:09:55 +0700

    add Info Vaccine Production Details

--- js/scripts_popup.js
+++ js/scripts_popup.js
@@ -14,19 +14,19 @@ $("#txtAboutData").change(function(){
 		$(this).val('');
 		$(document).keydown(function(e){if(e.keyCode == 27){$('#viewModalData').modal("hide");}});
 	}
-	// About Vaccine
-	if(target=="VaccineTechnology"){
-		$("#imgVaccine").attr("src", "https://www.atascientific.com.au/wp-content/uploads/2021/02/COVID-vaccine.jpg");
-		$("#viewModalVaccine").modal("toggle");
-		$(this).val('');
-		$(document).keydown(function(e){if(e.keyCode == 27){$('#viewModalVaccine').modal("hide");}});
-	}
-	// About Vaccine
+	// About Vaccine Details
 	if(target=="VaccineDetails"){
 		$("#imgVaccineDetails").attr("src", "https://ca-times.brightspotcdn.com/dims4/default/f083751/2147483647/strip/true/crop/4832x3222+0+0/resize/1486x991!/quality/90/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2F68%2Fd8%2Ff92f9ad365fa18a23b836492b207%2F1bd25766c1ef4fb7bea146b3eb1807bc");
 		$("#viewModalVaccineDetails").modal("toggle");
 		$(this).val('');
 		$(document).keydown(function(e){if(e.keyCode == 27){$('#viewModalVaccineDetails').modal("hide");}});
+	}
+	// About Vaccine Technology
+	if(target=="VaccineTechnology"){
+		$("#imgVaccine").attr("src", "https://www.atascientific.com.au/wp-content/uploads/2021/02/COVID-vaccine.jpg");
+		$("#viewModalVaccine").modal("toggle");
+		$(this).val('');
+		$(document).keydown(function(e){if(e.keyCode == 27){$('#viewModalVaccine').modal("hide");}});
 	}
 });
 
