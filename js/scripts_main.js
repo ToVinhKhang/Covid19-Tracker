625148c7b2ef1da1db3d6f7bdbe3224677e1a6f0
Kent-Khang <vinhkhang1969@gmail.com>
2021-07-21 16:37:42 +0700

    update lang

--- js/scripts_main.js
+++ js/scripts_main.js
@@ -5,7 +5,7 @@
 // -----------------------------------------------
 
 // Choose Language At First
-const lang = "./lang/vn.json";
+const lang = "./lang/en.json";
 
 // API for Countries
 const API_Countries = "https://corona.lmao.ninja/v2/countries";
