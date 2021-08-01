e2c8daa5f59539e8522ee8337b24fbfeb549c6be
Kent-Khang <vinhkhang1969@gmail.com>
2021-08-01 22:45:32 +0700

    update vaccine detail

--- js/scripts_detailVN.js
+++ js/scripts_detailVN.js
@@ -89,6 +89,7 @@ function displayCity(jsonData){
 	
 	displayTotalVN_Cases(totalCases);
 	displayTotalVN_Recovered(totalRecovered);
+	//displayTotalVN_Deaths(totalDeaths);
 	displayRate(totalCases,totalRecovered,totalDeaths);
 	
 	//Make sure focus max value at first
