8ff591af9f6df34e5bb6912472279706c9ce8af6
Kent-Khang <vinhkhang1969@gmail.com>
2021-08-08 02:18:43 +0700

    nice

--- js/serviceWorker.js
+++ js/serviceWorker.js
@@ -1,10 +1,7 @@
 if("serviceWorker" in navigator){
-	navigator.serviceWorker.register("sw.js").then(registration => {
-		console.log("SW Registered");
-		console.log(registration);
-	})
-	.catch(error => {
-		console.log("SW Registered Failed");
-		console.log(error);
-	})
+	navigator.serviceWorker.register("sw.js")
+		.then(registration => {
+			//console.log("PWA Success");
+		})
+		.catch(error => {console.log(error);})
 }
