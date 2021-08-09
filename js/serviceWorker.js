e9f81edb558ad106b0614b54a27d41f395076f62
Kent-Khang <vinhkhang1969@gmail.com>
2021-08-10 00:01:40 +0700

    test

--- js/serviceWorker.js
+++ js/serviceWorker.js
@@ -1,11 +1,7 @@
 if("serviceWorker" in navigator){
-	caches.keys().then(function(cacheNames){
-		cacheNames.forEach(function(cacheName){
-			caches.delete(cacheName);
-		});
-	});
-	
 	navigator.serviceWorker.register("sw.js")
-		.then(registration => {//console.log("PWA Success");})
+		.then(registration => {
+			//console.log("PWA Success");
+		})
 		.catch(error => {console.log(error);})
 }
