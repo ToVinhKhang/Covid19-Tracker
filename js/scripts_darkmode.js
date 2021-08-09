7d618765c6fdcf651cc991fe17eb2fbc9be83710
Kent-Khang <vinhkhang1969@gmail.com>
2021-08-09 15:00:19 +0700

    test

--- js/scripts_darkmode.js
+++ js/scripts_darkmode.js
@@ -3,12 +3,23 @@
 // Author: ToVinhKhang
 // Portfolio: https://tovinhkhang.netlify.app/
 // -----------------------------------------------
+var darkModeLocal = localStorage.getItem("theme");
+console.log(darkModeLocal)
 
 $("#darkMode").click(function() {
 	document.body.classList.toggle("dark-mode");
 	var darkMode = document.getElementById("darkMode").classList;
-	if(darkMode.contains("fa-moon-o") == true){
-		darkMode.toggle("fa-sun-o");darkMode.remove("fa-moon-o");}
-	else if(darkMode.contains("fa-moon-o") == false){
-		darkMode.toggle("fa-moon-o");darkMode.remove("fa-sun-o");}
+	
+	if(darkModeLocal == "dark"){
+		darkMode.toggle("fa-sun-o");
+		darkMode.remove("fa-moon-o");
+		console.log(darkModeLocal)
+		localStorage.setItem("theme","light");
+	}
+	else if(darkModeLocal == "light"){
+		darkMode.toggle("fa-moon-o");
+		darkMode.remove("fa-sun-o");
+		console.log(darkModeLocal)
+		localStorage.setItem("theme","dark");
+	}
 });
