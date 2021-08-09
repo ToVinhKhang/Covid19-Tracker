08a3de2fd9f4f41dbf96a17e85ef33afdd7da1df
Kent-Khang <vinhkhang1969@gmail.com>
2021-08-10 01:57:44 +0700

    prefetch

--- sw.js
+++ sw.js
@@ -1,4 +1,9 @@
-var fileToCache = [
+var CACHE_VERSION = 1;
+var CURRENT_CACHES = {
+	prefetch: 'window-cache-v' + CACHE_VERSION
+};
+
+var file = [
 	"./",
 	"./css/styles_main.css",
 	"./css/styles_responsive.css"
@@ -6,8 +11,8 @@ var fileToCache = [
 
 self.addEventListener("install", e=>{
 	e.waitUntil(
-		caches.open("static").then(cache =>{
-			return cache.addAll(fileToCache);
+		caches.open(CURRENT_CACHES.prefetch).then(cache =>{
+			return cache.addAll(file);
 		})
 	);
 });
@@ -16,6 +21,22 @@ self.addEventListener("fetch", e=>{
 	e.respondWith(
 		caches.match(e.request).then(response => {
 			return response || fetch(e.request);
+		})
+	);
+});
+
+self.addEventListener('activate', function(event) {
+	self.clients.claim();
+	var expectedCacheNames = Object.keys(CURRENT_CACHES).map(function(key){return CURRENT_CACHES[key];});
+	event.waitUntil(
+		caches.keys().then(function(cacheNames) {
+			return Promise.all(
+				cacheNames.map(function(cacheName){
+					if (expectedCacheNames.indexOf(cacheName) === -1){console.log('Deleting out of date cache:', cacheName);
+						return caches.delete(cacheName);
+					}
+				})
+			);
 		})
 	);
 });
