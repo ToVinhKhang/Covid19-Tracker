5ab6ee1119a2f91fa04185f59ddf2ed5604f68ee
Kent-Khang <vinhkhang1969@gmail.com>
2021-08-10 02:33:34 +0700

    donegit add . LGTM

--- sw.js
+++ sw.js
@@ -1,38 +1,41 @@
+// -----------------------------------------------
+// SERVICE WORKER - SW
+// Author: ToVinhKhang
+// Portfolio: https://tovinhkhang.netlify.app/
+// -----------------------------------------------
 
-var CURRENT_CACHES = {
-	prefetch: 'Covid-19 Tracker'
-};
-
+// Init
+var CURRENT_CACHES = {prefetch: 'Covid-19 Tracker'};
 var file = [
 	"./",
 	"./css/styles_main.css",
 	"./css/styles_responsive.css"
 ];
 
+// Add File Prefecth
 self.addEventListener("install", e=>{
 	e.waitUntil(
-		caches.open(CURRENT_CACHES.prefetch).then(cache =>{
-			return cache.addAll(file);
-		})
+		caches.open(CURRENT_CACHES.prefetch).then(cache =>{return cache.addAll(file);})
 	);
 });
 
+
+// Fetch file Prefetch
 self.addEventListener("fetch", e=>{
 	e.respondWith(
-		caches.match(e.request).then(response => {
-			return response || fetch(e.request);
-		})
+		caches.match(e.request).then(response => {return response || fetch(e.request);})
 	);
 });
 
+// Delete All Caches
 self.addEventListener('activate', e=>{
-	self.clients.claim();
-	var expectedCacheNames = Object.keys(CURRENT_CACHES).map(function(key){return CURRENT_CACHES[key];});
+	self.clients.claim(); var expectedCacheNames = Object.keys(CURRENT_CACHES).map(function(key){return CURRENT_CACHES[key];});
 	e.waitUntil(
 		caches.keys().then(function(cacheNames) {
 			return Promise.all(
 				cacheNames.map(function(cacheName){
-					if (expectedCacheNames.indexOf(cacheName) === -1){console.log('Deleting out of date cache:', cacheName);
+					if (expectedCacheNames.indexOf(cacheName) === -1){
+						console.log('Deleted Cache: ', cacheName);
 						return caches.delete(cacheName);
 					}
 				})
@@ -40,3 +43,6 @@ self.addEventListener('activate', e=>{
 		})
 	);
 });
+
+// End
+
