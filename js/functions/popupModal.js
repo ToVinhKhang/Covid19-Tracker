1786cbb27050edbd93e733809e01c1fab316b708
Kent-Khang <vinhkhang1969@gmail.com>
2021-08-14 18:37:43 +0700

    test

--- js/functions/popupModal.js
+++ js/functions/popupModal.js
@@ -55,8 +55,8 @@ $("#txtAboutData").change(function(){
 });
 
 //VNMAP
-$("#vnName").click(function(){ToggleOrHide("#viewModalMap");document.html.style.zoom = "90%";setTimeout(()=>{document.html.style.zoom = "100%";},2000);});
-$(".btnMapDiv").click(function(){ToggleOrHide("#viewModalMap");document.html.style.zoom = "90%";setTimeout(()=>{document.html.style.zoom = "100%";},2000);});
+$("#vnName").click(function(){ToggleOrHide("#viewModalMap");document.documentElement.style.zoom = "90%";setTimeout(()=>{document.documentElement.style.zoom = "100%";},2000);});
+$(".btnMapDiv").click(function(){ToggleOrHide("#viewModalMap");document.documentElement.style.zoom = "90%";setTimeout(()=>{document.documentElement.style.zoom = "100%";},2000);});
 
 
 function ToggleOrHide(idModal){
