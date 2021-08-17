ad6ab99bddd38d3c209e67ce0088cd900b3f005f
Kent-Khang <vinhkhang1969@gmail.com>
2021-08-18 01:45:35 +0700

    color

--- js/simpleMap/main.js
+++ js/simpleMap/main.js
@@ -81,7 +81,114 @@ var state_specific = {
     VNM512: {name: "Hà Giang"},
     VNM601: {name: "Lào Cai"}
 }
-var colors = ["#F7F7F7","#F7ED97","#FFDA47","#FFBD57","#FF3C00","#E80000"];
+var colors = ["#F7F7F7","#A1BFCF","#7FABCF","#24A6CF","#0D7197","#0D4164"];
+
+// Config
+var simplemaps_countrymap_mapdata = {
+	main_settings: {
+		width: "500", 
+		background_color: "white",
+		background_transparent: "no",
+		border_color: "rgb(204, 204, 204)",
+		state_description: "- None -",
+		state_color: "rgb(247, 247, 247)",
+		state_hover_color: "#17a2b8",
+		state_url: "",
+		border_size: 3,
+		all_states_inactive: "no",
+		all_states_zoomable: "no",
+		location_description: "Vietnam Map",
+		location_url: "",
+		location_color: "#FF0067",
+		location_opacity: 0.8,
+		location_hover_opacity: 1,
+		location_size: 25,
+		location_type: "square",
+		location_image_source: "frog.png",
+		location_border_color: "#FFF",
+		location_border: 2,
+		location_hover_border: 2.5,
+		all_locations_inactive: "no",
+		all_locations_hidden: "no",
+		label_color: "#d5ddec",
+		label_hover_color: "#d5ddec",
+		label_size: 22,
+		label_font: "Arial",
+		hide_labels: "no",
+		hide_eastern_labels: "no",
+		zoom: "yes",
+		manual_zoom: "yes",
+		back_image: "no",
+		initial_back: "no",
+		initial_zoom: "-1",
+		initial_zoom_solo: "no",
+		region_opacity: 1,
+		region_hover_opacity: 0.6,
+		zoom_out_incrementally: "yes",
+		zoom_percentage: 0.99,
+		zoom_time: 0.5,
+		popup_color: "white",
+		popup_opacity: 0.9,
+		popup_shadow: 1,
+		popup_corners: 5,
+		popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+		popup_nocss: "no",
+		div: "map",
+		auto_load: "yes",
+		url_new_tab: "no",
+		images_directory: "default",
+		fade_time: 0.1,
+		link_text: "View Website",
+		popups: "detect",
+		state_image_url: "",
+		state_image_position: "",
+		location_image_url: ""
+	},
+	state_specific,
+	locations: {},labels: {},
+	legend: {
+		entries: [{
+				name: "0",
+				color: colors[0],
+				type: "location",
+				shape: "square",
+				ids: ""
+			},{
+				name: "1-9",
+				color: colors[1],
+				type: "location",
+				shape: "square",
+				ids: ""
+			},{
+				name: "10-99",
+				color: colors[2],
+				type: "location",
+				shape: "square",
+				ids: ""
+			},{
+				name: "100-999",
+				color: colors[3],
+				type: "location",
+				shape: "square",
+				ids: ""
+			},{
+				name: "1000-9999",
+				color: colors[4],
+				type: "location",
+				shape: "square",
+				ids: ""
+			},{
+				name: ">10000",
+				color: colors[5],
+				type: "location",
+				shape: "square",
+				ids: ""
+			}
+		]
+	},
+	regions: {}
+};
+
 
 // Display
 function displayVNMap(detail){
@@ -98,6 +205,7 @@ function displayVNMap(detail){
 		}
     }
 }
+
 // Set
 function setColor(n){
     if(n >= 1 && n <= 10){return colors[1];}
@@ -108,6 +216,8 @@ function setColor(n){
 	else{return colors[0];}
 }
 
-// END
+// Run
+async function OK(){await getVNMap();simplemaps_countrymap_mapdata.state_specific = state_specific;simplemaps_countrymap.refresh();}
+OK();
 
-
+// End
