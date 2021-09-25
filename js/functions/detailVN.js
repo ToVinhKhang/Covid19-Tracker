0695635a573b2e86e4b147f6bc96e368d5bb1ac9
Kent-Khang <vinhkhang1969@gmail.com>
2021-09-25 18:57:27 +0700

    nothing :D

--- js/functions/detailVN.js
+++ js/functions/detailVN.js
@@ -216,7 +216,7 @@ async function displayDailyVietnam(jsonData,label1,label2){
 	createChart(dateArray,deathsArray_New,label2,"#E41E20","newdeathsChart","line","newdeathsChartDiv");
 }
 
-var population = 98350000;
+var population = 98450000;
 
 function displayDailyVaccines(jsonData,label1,label2){
 	var dateArray = [];
